187047340d7b41faa0606ba693472f8f4d64b203
Marcos Toranzo <marcos.toranzo11@gmail.com>
2023-03-14 14:44:01 +0100

    Improve AppLogo tests

--- test/widget_tests/app_logo_test.dart
+++ test/widget_tests/app_logo_test.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_test_app/widgets/app_logo.dart';
+import 'package:flutter_test_app/widgets/cutom_text.dart';
+import 'package:flutter_test_app/widgets/space.dart';
 
 void main() {
   testWidgets('AppLogo should not show text', (tester) async {
@@ -20,14 +22,10 @@ void main() {
     expect(textFinder, findsOneWidget);
   });
 
-  testWidgets('AppLogo should show correct structure', (tester) async {
+  testWidgets('AppLogo should show correct image', (tester) async {
     const size = 100.0;
 
     await tester.pumpWidget(_getAppLogo(size: size));
-
-    final columnFinder = find.byType(Column);
-
-    expect(columnFinder, findsOneWidget);
 
     final svgPictureFinder = find.byType(SvgPicture);
 
@@ -43,6 +41,24 @@ void main() {
     );
     expect(svgPicture.height, size);
     expect(svgPicture.width, size);
+  });
+
+  testWidgets('AppLogo should show correct structure', (tester) async {
+    const size = 100.0;
+
+    await tester.pumpWidget(_getAppLogo(size: size));
+
+    final columnFinder = find.byType(Column);
+
+    expect(columnFinder, findsOneWidget);
+
+    final column = tester.widget(columnFinder) as Column;
+
+    expect(column.children.length, 3);
+
+    expect(column.children[0] is SvgPicture, true);
+    expect(column.children[1] is Space, true);
+    expect(column.children[2] is OnBackgroundText, true);
   });
 }
 
